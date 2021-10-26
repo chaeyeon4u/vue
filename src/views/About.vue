cf3b1090ac2c87821191981bdbcee3619544bf37
chaeyeon4u <cy990426@naver.com>
2021-10-26 21:32:51 +0900

    20211026

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <div class="card-header">
+        About
+    </div>
+    <div class="card-body">
+        <div class="about">
+          <h1>This is an about page</h1>
+        </div>
+    </div>
   </div>
+
 </template>
+
+<script>
+//이거 안만들면, 실행시키기 전에 자동적으로 추가된다.
+export default {
+  name: 'Abuout',
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card">
+    <div class="card-header">
+      Home View
+    </div>
+    <div class="card-body">
+      <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    test() {
+      var var1 = 1;
+      var var2 = 2;
+      console.log(var2);
+    },
+  },
+};
 </script>
